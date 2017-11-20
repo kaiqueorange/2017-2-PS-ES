52d4bdfe0002bb1eb60b7ae268232a42d30ee9ab
Márcio Flores <marciogabriel07@gmail.com>
2017-11-20 21:54:51 -0200

    Configuração de logs

--- 4.Sistema/LOCCARR/app/src/main/java/br/com/flores/marcio/loccarr/MainActivity.java
+++ 4.Sistema/LOCCARR/app/src/main/java/br/com/flores/marcio/loccarr/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -19,29 +20,39 @@ public class MainActivity extends AppCompatActivity {
     EditText editMail, editSenha;
     String mail, senha;
     FirebaseAuth firebaseAuth;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d("TAG", "OnCreate método");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         editMail = findViewById(R.id.editMail);
         editSenha = findViewById(R.id.editSenha);
+
+        progressDialog = new ProgressDialog(this);
     }
 
     public void entrar(View view){
+        Log.d("TAG", "Entrar método");
         mail = editMail.getText().toString().trim();
         senha = editSenha.getText().toString().trim();
 
         if (TextUtils.isEmpty(mail)){
+            Log.d("TAG", "E-mail em branco");
             Toast.makeText(this, "E-mail inválido.", Toast.LENGTH_SHORT).show();
             return;
         }
 
         if (TextUtils.isEmpty(senha)){
+            Log.d("TAG", "Senha em branco");
             Toast.makeText(this, "Senha inválida.", Toast.LENGTH_SHORT).show();
             return;
         }
+
+        progressDialog.setMessage("Entrando...");
+        progressDialog.show();
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -50,12 +61,22 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()){
+                            Log.d("TAG", "Login realizado");
                             finish();
+                            progressDialog.dismiss();
                             startActivity(new Intent(getApplicationContext(), Dashboard.class));
                         } else {
+                            Log.d("TAG", "Login não realizado");
                             Toast.makeText(MainActivity.this, "Usuário/senha inválidos.", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
                         }
                     }
                 });
+    }
+
+    public void cadastrar (View view){
+        Log.d("TAG", "Cadastrar método");
+        finish();
+        startActivity(new Intent(getApplicationContext(), Cadastrar.class));
     }
 }
